4e581e00bf39f9e2480a5e0d70f4ec7f3051cca4
user <mithunmits50@gmail.com>
2022-09-27 21:39:02 +0530

    testing

--- src/main/java/com/zenfra/service/ReportService.java
+++ src/main/java/com/zenfra/service/ReportService.java
@@ -6,10 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.zenfra.dto.AddReportColumnsRequest;
 import com.zenfra.dto.DeleteReportColumnsRequest;
